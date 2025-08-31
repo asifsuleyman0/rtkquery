063b204f0b86342a9218084764acad1b15da786a
asifsuleymanov0 <suleymanovasif000@gmail.com>
2025-09-01 01:43:34 +0400

    cacacd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Tailwind artıq require() vasitəsi ilə çağırılacaq PostCSS pluginində işləyir
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js',
+    postcss: './postcss.config.cjs', // cjs olmalıdır
   },
 });
