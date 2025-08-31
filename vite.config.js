795daaeb783d51c9c5f2d8dbab08b27f95eaad37
asifsuleymanov0 <suleymanovasif000@gmail.com>
2025-09-01 01:38:45 +0400

    cacadc

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Tailwind artıq require() vasitəsi ilə çağırılacaq PostCSS pluginində işləyir
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
+    postcss: './postcss.config.js',
   },
 });
